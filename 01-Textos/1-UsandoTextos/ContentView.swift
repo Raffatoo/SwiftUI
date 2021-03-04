f49e70827375102573e327b8ee4a42ca9f6744f6
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2021-03-03 20:53:05 -0600

    Textos
    UILabel UKit  ->  Text SwiftUI

---
+++ 01-Textos/1-UsandoTextos/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  1-UsandoTextos
+//
+//  Created by Cruz Torres on 25/06/20.
+//  Copyright © 2020 Cruz Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+        
+            Text("Hello, World!")
+            Text("Hela, Mundo!")
+            Text("Ciao Mondo!")
+            Text("Salut Monde!")
+            Text("Hallo Welt!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
